55944f529312bb4ea5bdfaf15c7fb0a41bca0e39
octaviantepsan <tepsanoctavian@gmail.com>
2024-10-17 19:31:56 +0300

    Made functions to process the data which is to be inserted into the db.

--- backend/main.js
+++ backend/main.js
@@ -23,7 +23,7 @@ db.getConnection((err, connection) => {
 });
 
 function queryDb() {
-    const query = 'SELECT * FROM users'; 
+    const query = 'SELECT * FROM users';
 
     db.query(query, (err, results) => {
         if (err) {
@@ -33,9 +33,14 @@ function queryDb() {
     });
 }
 
-function testInsert() {
+function testInsertUser(firstName, lastName, city, adress) {
     const query = `INSERT INTO Users (LastName, FirstName, City, Address, DateCreated, IsVendor, Rating, Sales) 
-                    VALUES ('Tepsan', 'Dan', 'Bucuresti', 'Mare', '2024-10-11', 1, 0, 0)`;
+                   VALUES ('Tepsan', 'Dan', 'Bucuresti', 'Mare', '2024-10-11', 1, 0, 0)`;
+
+    if(processUserData(firstName, lastName, city, adress) === false) {
+        console.log("Data was processed incorrectly");
+        return;
+    }
     
     db.query(query, (err, results) => {
         if (err) {
@@ -45,10 +50,32 @@ function testInsert() {
     });
 }
 
+function processUserData(firstName, lastName, city, adress) {
+    let letterOnlyCheck = /^[a-zA-Z]+$/;
+
+    if ((typeof (firstName) === 'string') && (typeof (lastName) === 'string') && (typeof (city) === 'string') && (typeof (adress) === 'string')) {
+        if (letterOnlyCheck.test(firstName) && letterOnlyCheck.test(lastName) && letterOnlyCheck.test(city))
+            return true;
+    }
+
+    return false;
+}
+
+function processItemData(itemName, itemType, stock) {
+    let letterOnlyCheck = /^[a-zA-Z]+$/;
+
+    if ((typeof (itemName) === 'string') && (typeof (itemType) === 'string') && (typeof (stock) === 'number')) {
+        if (letterOnlyCheck.test(itemType))
+            return true;
+    }
+
+    return false;
+}
+
 // Start the server
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
 
 //queryDb();
-testInsert();
+testInsertUser();
