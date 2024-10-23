e5b7ee3b677a0d4821a3dc259a1d36fe81a5a26f
octaviantepsan <tepsanoctavian@gmail.com>
2024-10-23 18:46:21 +0300

    Finished insert/delete functions for users and items.

--- backend/main.js
+++ backend/main.js
@@ -32,16 +32,77 @@ function queryDb() {
         console.log(results);
     });
 }
+    // ------------------------------------------------------
+        //USER FUNCTIONS
+    // ------------------------------------------------------
+
+function processUserData(firstName, lastName, city, adress) {
+    let letterOnlyCheck = /^[a-zA-Z]+$/;
+
+    if ((typeof (firstName) != 'string') || (typeof (lastName) != 'string') || (typeof (city) != 'string') || (typeof (adress) != 'string')) {
+        return false;
+    }
+
+    if (letterOnlyCheck.test(firstName) && letterOnlyCheck.test(lastName) && letterOnlyCheck.test(city)) {
+        return true;
+    }
+        
+    return false;
+}
+
+function deleteUser(userId) {
+    const query = `DELETE FROM Users WHERE UserId = ${userId}`;
+    console.log("User has been deleted.");
+}
 
 function testInsertUser(firstName, lastName, city, adress) {
-    const query = `INSERT INTO Users (LastName, FirstName, City, Address, DateCreated, IsVendor, Rating, Sales) 
-                   VALUES ('Tepsan', 'Dan', 'Bucuresti', 'Mare', '2024-10-11', 1, 0, 0)`;
-
-    if(processUserData(firstName, lastName, city, adress) === false) {
-        console.log("Data was processed incorrectly");
+    if (processUserData(firstName, lastName, city, adress) === false) {
+        console.error("Data was processed incorrectly");
         return;
     }
-    
+
+    const query = `INSERT INTO Users (LastName, FirstName, City, Address)
+                   VALUES ('${lastName}', '${firstName}', '${city}', '${adress}')`;
+
+    db.query(query, (err, results) => {
+        if (err) {
+            console.error(err.message);
+        }
+        console.log(results);
+    });
+}
+    // ------------------------------------------------------
+        //ITEM FUNCTIONS
+    // ------------------------------------------------------
+
+function processItemData(itemName, itemType, stock = 0) {
+    let letterOnlyCheck = /^[a-zA-Z]+$/;
+
+    if ((typeof (itemName) != 'string') || (typeof (itemType) != 'string') || (typeof (stock) != 'number')) {
+        return false;
+    }
+
+    if (letterOnlyCheck.test(itemType) && stock >= 0) {
+        return true;
+    }
+        
+    return false;
+}
+
+function deleteItem(itemId) {
+    const query = `DELETE FROM Items WHERE ItemId = ${itemId}`;
+    console.log("Item has been deleted.");
+}
+
+function testInsertItem(itemName, itemType, stock, userId) {
+    if (processItemData(itemName, itemType, stock) === false) {
+        console.error("Data was processed incorrectly");
+        return;
+    }
+
+    const query = `INSERT INTO Items (ItemName, ItemType, Stock, UserId)
+                   VALUES ('${itemName}', '${itemType}', '${stock}', ${userId})`;
+
     db.query(query, (err, results) => {
         if (err) {
             console.error(err.message);
@@ -50,32 +111,7 @@ function testInsertUser(firstName, lastName, city, adress) {
     });
 }
 
-function processUserData(firstName, lastName, city, adress) {
-    let letterOnlyCheck = /^[a-zA-Z]+$/;
-
-    if ((typeof (firstName) === 'string') && (typeof (lastName) === 'string') && (typeof (city) === 'string') && (typeof (adress) === 'string')) {
-        if (letterOnlyCheck.test(firstName) && letterOnlyCheck.test(lastName) && letterOnlyCheck.test(city))
-            return true;
-    }
-
-    return false;
-}
-
-function processItemData(itemName, itemType, stock) {
-    let letterOnlyCheck = /^[a-zA-Z]+$/;
-
-    if ((typeof (itemName) === 'string') && (typeof (itemType) === 'string') && (typeof (stock) === 'number')) {
-        if (letterOnlyCheck.test(itemType))
-            return true;
-    }
-
-    return false;
-}
-
 // Start the server
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
-
-//queryDb();
-testInsertUser();
