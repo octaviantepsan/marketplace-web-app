604107561db6356f86885b88f755b49fadf835ec
octaviantepsan <tepsanoctavian@gmail.com>
2024-10-24 17:38:55 +0300

    started angular

--- frontend/site/src/app/app.component.ts
+++ frontend/site/src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SigninComponent } from './pages/signin/signin.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, SigninComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'site';
+  isSignUpVisible: boolean;
+
+  constructor() {
+    this.isSignUpVisible = false;
+  }
 }
---
+++ frontend/site/src/app/pages/userpanel/userpanel.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-userpanel',
+  standalone: true,
+  imports: [],
+  templateUrl: './userpanel.component.html',
+  styleUrl: './userpanel.component.css'
+})
+export class UserpanelComponent {
+
+}
